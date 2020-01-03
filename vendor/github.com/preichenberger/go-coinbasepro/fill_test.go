9ef9e93e229a06ab716648344f78b3b68cfe305a
kerim_oktay <yaso195@yahoo.com>
2020-01-03 12:44:08 -0800

    Added new libraries to the vendor

---
+++ vendor/github.com/preichenberger/go-coinbasepro/fill_test.go
@@ -0,0 +1,26 @@
+package coinbasepro
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestListFills(t *testing.T) {
+	var fills []Fill
+	client := NewTestClient()
+	params := ListFillsParams{
+		ProductID: "BTC-USD",
+	}
+	cursor := client.ListFills(params)
+	for cursor.HasMore {
+		if err := cursor.NextPage(&fills); err != nil {
+			t.Error(err)
+		}
+
+		for _, f := range fills {
+			if StructHasZeroValues(f) {
+				t.Error(errors.New("Zero value"))
+			}
+		}
+	}
+}
---
+++ vendor/github.com/preichenberger/go-coinbasepro/report_test.go
@@ -0,0 +1,31 @@
+package coinbasepro
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestCreateReportAndStatus(t *testing.T) {
+	// # DISABLED in sandbox
+	return
+	client := NewTestClient()
+	newReport := Report{
+		Type:      "fill",
+		StartDate: time.Now().Add(-24 * 4 * time.Hour),
+		EndDate:   time.Now().Add(-24 * 2 * time.Hour),
+	}
+
+	report, err := client.CreateReport(&newReport)
+	if err != nil {
+		t.Error(err)
+	}
+
+	currentReport, err := client.GetReportStatus(report.ID)
+	if err != nil {
+		t.Error(err)
+	}
+	if StructHasZeroValues(currentReport) {
+		t.Error(errors.New("Zero value"))
+	}
+}
